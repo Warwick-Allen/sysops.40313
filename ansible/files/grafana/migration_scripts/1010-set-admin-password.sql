c41dddc76a31a6451abdadb8b8219bf69deb10a5
Warwick Allen <warwick@remarque.co.nz>
2024-03-27 04:26:54 +1300

    WiP grafana-config

--- ansible/files/grafana/migration_scripts/1010-set-admin-password.sql
+++ ansible/files/grafana/migration_scripts/1010-set-admin-password.sql
@@ -1,11 +1,11 @@
 -- Change the admin password to "adm1n".
 
 UPDATE
-  user
+  `user`
 SET
-  `password` = 'f53ea24bede17bfa17968693b3bf27d4be6af80de12a236c93204a0abaffb5be7e1f0c596e3662d22672cf5ed57ed988fe16',
-  `updated` = '2024-03-24 09:05:49',
-  `last_seen_at` = '2024-03-24 09:05:49'
+  `password` = '02eff6506b8b21bfa2e20fe0a9edfdfe5bab17a8276cd9a6a1a5e8afae6129ff8d911d8a66a7238876aae9a6c2c33e75c42d',
+  `updated` = CURRENT_TIMESTAMP,
+  `last_seen_at` = CURRENT_TIMESTAMP
 WHERE
   `id` = 1;
 
@@ -15,15 +15,15 @@ VALUES
   (
     1,
     1,
-    '5a67da18ff368b328f4f04ceedeeed21adbe3fc8a6ff2a226c27c1c1c2de88fc',
-    '5a67da18ff368b328f4f04ceedeeed21adbe3fc8a6ff2a226c27c1c1c2de88fc',
+    'e7708855adedeb2e561ac876f0ba8edac7db47fd8ea1f693740f4467741c7500',
+    'e7708855adedeb2e561ac876f0ba8edac7db47fd8ea1f693740f4467741c7500',
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0',
-    '172.23.0.1',
+    '172.20.0.1',
     1,
-    1711271149,
-    1711271127,
-    1711271127,
-    1711271127,
+    {{ ansible_date_time.epoch }},
+    {{ ansible_date_time.epoch }},
+    {{ ansible_date_time.epoch }},
+    {{ ansible_date_time.epoch }},
     0
   );
 
@@ -31,16 +31,3 @@ INSERT INTO
   sqlite_sequence
 VALUES
   ('user_auth_token', 1);
-
----
-
-UPDATE `user`
-SET
-  `password` = '02eff6506b8b21bfa2e20fe0a9edfdfe5bab17a8276cd9a6a1a5e8afae6129ff8d911d8a66a7238876aae9a6c2c33e75c42d'
-, `created` ='2024-03-26 06:40:41'
-, `updated` ='2024-03-26 06:43:58'
-, `last_seen_at` ='2024-03-26 06:43:58'
-WHERE
-`id`=1
-;
-
